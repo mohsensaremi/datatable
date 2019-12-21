a952482e38b424aad74b0a95f7df53670a5b1000
mohsen <mohsen@saeeds-MacBook-Pro.local>
2019-12-21 16:49:58 +0330

    add custom pagination actions

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,68 @@
 import React from 'react';
 import MuiTable from '@material-ui/core/Table';
 import TablePagination from '@material-ui/core/TablePagination';
+import IconButton from '@material-ui/core/IconButton';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import TableHead from '../TableHead';
 import TableBody from '../TableBody';
 import {SettingsContext, NetworkContext} from "../../context";
+
+const TablePaginationActions = (props) => {
+    const style = {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+    };
+    const totalPages = Math.ceil(props.count / props.rowsPerPage);
+
+    const loadingJsx = (
+
+        <Grid
+            container
+            alignItems={"center"}
+            justify={"center"}
+            style={style}
+        >
+            <CircularProgress
+                size={16}
+            />
+        </Grid>
+    );
+
+    return (
+        <NetworkContext.Consumer>
+            {
+                ({loading}) => (
+                    <div className={props.className}>
+                        <IconButton
+                            disabled={props.page === 0 || loading}
+                            onClick={event => props.onChangePage(event, props.page - 1)}
+                        >
+                            <ChevronRightIcon
+                                {...props.backIconButtonProps}
+                            />
+                            {loading && loadingJsx}
+                        </IconButton>
+                        <IconButton
+                            disabled={props.page >= totalPages - 1 || loading}
+                            onClick={event => props.onChangePage(event, props.page + 1)}
+                        >
+                            <ChevronLeftIcon
+                                {...props.nextIconButtonProps}
+                            />
+                            {loading && loadingJsx}
+                        </IconButton>
+                    </div>
+                )
+            }
+        </NetworkContext.Consumer>
+    );
+}
 
 const Table = (props) => {
 
@@ -51,6 +110,7 @@ const Table = (props) => {
                                             }}
                                             onChangePage={onChangePage}
                                             onChangeRowsPerPage={onChangeRowsPerPage}
+                                            ActionsComponent={TablePaginationActions}
                                         />
                                     )
                                 }
